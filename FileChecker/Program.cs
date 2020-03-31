b99f9d828460e7ea5571b0057d3c8477103a89d7
Kenny <Kenny@KENNY-PC>
2020-03-31 14:43:02 +0800

    First commit

--- FileChecker/Program.cs
+++ FileChecker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileChecker
 {
@@ -10,6 +11,16 @@ namespace FileChecker
     {
         static void Main(string[] args)
         {
+            string homeDir = AppDomain.CurrentDomain.BaseDirectory;
+            string PathIni = Path.Combine(homeDir, "Path.ini");
+            string databaseFile = Path.Combine(homeDir, "data.sqlite");
+            if (!File.Exists(PathIni)) 
+            {
+                Console.WriteLine("Path.ini not found...");
+                Console.ReadKey();
+                return;
+            }
+
         }
     }
 }
